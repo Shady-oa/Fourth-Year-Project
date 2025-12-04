2b4a7b095a2a9e335f0ca7988e80d953261dd3f4
Shady Oa <shady@example.com>
2025-12-04 16:15:56 +0300

    theme toggle

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:final_project/Constants/colors.dart';
-import 'package:final_project/Constants/typograpy.dart';
 import 'package:final_project/SecondaryScreens/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   // Initialize Firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   // Run your app
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
-//
 class MyApp extends StatelessWidget {
   bool name() {
     return true;
@@ -24,11 +28,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: primaryBg,
-        textTheme: kTextTheme,
-      ),
-      home: SplashScreen(), //MainPage(), //main page
+      theme: Provider.of<ThemeProvider>(context).currentTheme,
+      home: SplashScreen(),
     );
   }
 }
